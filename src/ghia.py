ebcaf438efdfd0b8d8663ec8e7cde5e2ea8dbc67
PetrB <petr.budik96@gmail.com>
2019-09-28 00:16:47 +0200

    Update ghia.py

--- src/ghia.py
+++ src/ghia.py
@@ -1,25 +1,6 @@
 import click
 import requests
-
-@click.command()
-@click.argument('source', nargs=-1, required=True)
-@click.argument('target', required=True)
-def cp(source, target):
-	for s in source:
-		click.echo(f'Copying {s} to {target}')
-
-
-@click.command()
-@click.option('--count', required=True, type=int, help='Number of greetings.')
-@click.option('--name', '-n', prompt='Enter name: ', help='Name to greet', metavar='NAME')
-@click.option('-c/-C', '--color/--no-color')
-@click.option('-v', '--verbose', is_flag=True)
-def hello(count, name, color, verbose):
-	greeting = (f'Hello, {name}')
-	if color:
-		greeting = click.style(greeting, fg='green', bg='black')
-	for x in range(count):
-		click.echo(greeting)
+import configparser
 
 @click.command()
 @click.option('-s', '--strategy', type=click.Choice(['append', 'set', 'change'], case_sensitive=False), default='append', help='How to handle assignment collisions.', show_default=True)
@@ -29,8 +10,19 @@ def hello(count, name, color, verbose):
 @click.argument('REPOSLUG', required=True)
 def ghia(strategy, dry_run, config_auth, config_rules, reposlug):
 	"""CLI tool for automatic issue assigning of GitHub issues"""
+	
+	"Get configuration"
+	authConfig = configparser.ConfigParser()
+	authConfig.read_file(config_auth)
+	
+	ruleConfig = configparser.ConfigParser()
+	ruleConfig.read_file(config_rules)
+
+	testConfig = f'{authConfig["github"]["token"]}, {ruleConfig}'
+	click.style(testConfig, fg='green', bg='black')
+	click.echo(testConfig)
 	print(f'{strategy}, {dry_run}, {reposlug}')
 	
 # Toto bude použito při zavolání z CLI
 if __name__ == '__main__':
-	ghia()
+	ghia()	# pylint: disable=no-value-for-parameter
