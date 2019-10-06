841309c9b3f9bb7e13804a270f26e8bd2c931927
budikpet <sgt.petrov@gmail.com>
2019-10-06 13:06:29 +0200

    Code refactored.

--- src/ghia.py
+++ src/ghia.py
@@ -4,67 +4,81 @@ import re
 import sys
 import json
 import validator
-from strategy import Strategies, GhiaContext
+from strategy import Strategies, GhiaContext, GhiaStrategy, GroupedUsers, UserStatus
 from enum import Enum
 from configData import ConfigData
-
-class UserStatus(Enum):
-	ADD = "+"
-	REMOVE = "-"
-	LEAVE = "="	
 
 def hasFallbackLabel(issue, context):
 	existingLabels = [label for label in issue["labels"] if label["name"] == context.getFallbackLabel()]
 	return len(existingLabels) > 0
 
-def writeOutput(context: GhiaContext, issue, users):
-	symbol = lambda currSymbol, color: click.style(currSymbol, bold=True, fg=color)
-	info = f'{context.reposlug}#{issue["number"]}'
-	click.echo(f'-> {click.style(info, bold=True)} ({issue["html_url"]})')
+def getOutputData(context: GhiaContext, issue, groupedUsers: GroupedUsers):
+	data = None
+	
+	if groupedUsers.updateNeeded():
+		data = {
+			"assignees": groupedUsers.getUsersToAssign()
+		}
+	elif context.getFallbackLabel() != "" and not hasFallbackLabel(issue, context):
+		labels = [context.getFallbackLabel()]
+		labels.extend([label["name"] for label in issue["labels"]])
+		data = {
+			"labels": labels
+		}
 
+	if data is not None:
+		data = json.dumps(data)
+
+	return data
+
+def updateIssue(context: GhiaContext, issue, groupedUsers: GroupedUsers):
 	# Update issue
 	statusCode = 200
-	changes = False
-
-	assignees = list()
-	for user in users:
-		assignees.append(user[1])
-		if user[0] != UserStatus.LEAVE:
-			changes = True
 
 	if not context.dry_run:
-		if changes:
-			data = {
-				"assignees": list({user[1] for user in users if user[0] != UserStatus.REMOVE})
-			}
-			data = json.dumps(data)
-			r = context.session.patch(f'{context.base}/repos/{context.reposlug}/issues/{issue["number"]}', data=data)
-			statusCode = r.status_code	
-		elif context.getFallbackLabel() != "" and not hasFallbackLabel(issue, context):
-			labels = [context.getFallbackLabel()]
-			labels.extend([label["name"] for label in issue["labels"]])
-			data = {
-				"labels": labels
-			}
-			data = json.dumps(data)
+		data = getOutputData(context, issue, groupedUsers)
+		
+		if data is not None:
 			r = context.session.patch(f'{context.base}/repos/{context.reposlug}/issues/{issue["number"]}', data=data)
 			statusCode = r.status_code
+
+	return statusCode
+
+def writeUser(userStatus: UserStatus, user: str):
+	symbol = lambda currSymbol, color: click.style(currSymbol, bold=True, fg=color)
+	color = ""
+
+	if userStatus == UserStatus.ADD:
+		color = "green"
+	elif userStatus == UserStatus.LEAVE:
+		color = "blue"
+	else:
+		color = "red"
+
+	click.echo(f'   {symbol(userStatus.value, color)} {user}')
+
+def writeLabel(issue, context: GhiaContext):
+	msg = ""
+	if not hasFallbackLabel(issue, context):
+		msg = "added label"
+	else:
+		msg = "already has label"
+	click.echo(f'   {click.style("FALLBACK", fg="yellow")}: {msg} \"{context.getFallbackLabel()}\"')
+
+def writeOutput(context: GhiaContext, issue, groupedUsers: GroupedUsers, statusCode):
+	info = f'{context.reposlug}#{issue["number"]}'
+	click.echo(f'-> {click.style(info, bold=True)} ({issue["html_url"]})')
 	
 	# Create output
 	if statusCode != 200 and not context.dry_run:
 		# Error occured
 		click.echo(f'   {click.style("ERROR", fg="red")}: Could not update issue {info}', err=True)
 	else:
-		if len(users) != 0:
-			for (userStatus, user) in users:
-				click.echo(f'   {symbol(userStatus.value, "green")} {user}')
+		if groupedUsers.hasUsers() != 0:
+			for (userStatus, user) in groupedUsers.getOutputList():
+				writeUser(userStatus, user)
 		elif context.getFallbackLabel() != "":
-			msg = ""
-			if not hasFallbackLabel(issue, context):
-				msg = "added label"
-			else:
-				msg = "already has label"
-			click.echo(f'   {click.style("FALLBACK", fg="yellow")}: {msg} \"{context.getFallbackLabel()}\"')
+			writeLabel(issue, context)
 	
 	print
 
@@ -89,34 +103,20 @@ def patternMatches(issue, location, pattern):
 
 # Checks current issue against all patterns
 # Returns: [(userStatus, user), ...]
-def separateUsers(context: GhiaContext, issue):
+def groupUsers(context: GhiaContext, issue):
 	alreadyAssignedUsers = set(map(lambda assignee: assignee["login"], issue["assignees"]))
-	usersToAssign = set()
+	usersAutoMatched = set()
 	
 	# Find users that should be assigned
 	for (location, pairs) in context.getUserPatterns().items():
 		for (pattern, username) in pairs:
 			if patternMatches(issue, location, pattern):
 				# Current user should be added to the issue
-				usersToAssign.add(username)
+				usersAutoMatched.add(username)
 
 	# Prepare output
-	output = list()
-	fun = lambda userStatus, currSet: [(userStatus, username) for username in currSet]
-	if strategy == "append":
-		output = fun(UserStatus.ADD, usersToAssign.difference(alreadyAssignedUsers))
-		output.extend(fun(UserStatus.LEAVE, alreadyAssignedUsers))
-	elif strategy == "set":
-		output = fun(UserStatus.ADD, usersToAssign)
-		output.extend(fun(UserStatus.LEAVE, alreadyAssignedUsers))
-	elif strategy == "change":
-		output = fun(UserStatus.ADD, usersToAssign.difference(alreadyAssignedUsers))
-		output.extend(fun(UserStatus.REMOVE, alreadyAssignedUsers.difference(usersToAssign)))
-		output.extend(fun(UserStatus.LEAVE, alreadyAssignedUsers.intersection(usersToAssign)))
+	return context.strategy.getGroupedUsers(usersAutoMatched, alreadyAssignedUsers)
 	
-	output.sort(key=lambda pair: pair[1].lower())
-	return output
-
 inputStrategies = [strategy.name.lower() for strategy in Strategies]
 
 @click.command()
@@ -158,8 +158,9 @@ def ghia(strategy, dry_run, config_auth, config_rules, reposlug):
 		# Check all received issues
 		for issue in issues:
 			# Check current issue against all patterns in all locations
-			users = separateUsers(context, issue)
-			writeOutput(context, issue, users)
+			groupedUsers = groupUsers(context, issue)
+			statusCode = updateIssue(context, issue, groupedUsers)
+			writeOutput(context, issue, groupedUsers, statusCode)
 					
 		# Next page
 		context.session.params['page'] += 1
--- src/strategy.py
+++ src/strategy.py
@@ -2,18 +2,47 @@ from enum import Enum
 from configData import ConfigData
 import requests
 import click
-from typing import Set
+from typing import Set, List
+from dataclasses import dataclass, field
+
+class UserStatus(Enum):
+    ADD = "+"
+    LEAVE = "="
+    REMOVE = "-"
+
+@dataclass
+class GroupedUsers():
+    usersAutoMatched: Set[str] = field(default_factory=set)
+    usersToLeave: Set[str] = field(default_factory=set)
+    usersToRemove: Set[str] = field(default_factory=set)
+
+    def updateNeeded(self) -> bool:
+        return len(self.usersAutoMatched) > 0 or len(self.usersToRemove) > 0
+
+    def getUsersToAssign(self) -> Set[str]:
+        return list(self.usersAutoMatched | self.usersToLeave)
+
+    def getOutputList(self) -> List[str]:
+        outputList = [(UserStatus.ADD, user) for user in list(self.usersAutoMatched)]
+        outputList.extend([(UserStatus.LEAVE, user) for user in list(self.usersToLeave)])
+        outputList.extend([(UserStatus.REMOVE, user) for user in list(self.usersToRemove)])
+        
+        return sorted(outputList, key=lambda pair: pair[1].lower())
+
+    def hasUsers(self) -> bool:
+        return len(self.usersAutoMatched) > 0 or len(self.usersToRemove) > 0 or len(self.usersToLeave) > 0
+
 
 class GhiaContext():
-    def __init__(self, base, strategy, dry_run, config_auth, config_rules, reposlug):
-        self.base = base
-        self.strategy: GhiaStrategy = strategy
-        self.dry_run = dry_run
-        self.reposlug = reposlug
-        self.session = None
+    def __init__(self, base, strategy: str, dry_run, config_auth, config_rules, reposlug):
+        self.base: str = base
+        self.strategy: GhiaStrategy = Strategies[strategy.upper()].value
+        self.dry_run: bool = dry_run
+        self.reposlug: str = reposlug
+        self.session: requests.session = None
 
         # Get configuration
-        self.configData = ConfigData(config_auth, config_rules)
+        self.configData: ConfigData = ConfigData(config_auth, config_rules)
 
     def getUserPatterns(self):
         return self.configData.userPatterns
@@ -25,21 +54,26 @@ class GhiaContext():
         return self.configData.fallbackLabel
 
 class GhiaStrategy():
-    def getSeparatedUsers(self, usersToAssign: Set[str], usersAlreadyAssigned: Set[str]):
+    def getGroupedUsers(self, usersAutoMatched: Set[str], usersAlreadyAssigned: Set[str]) -> GroupedUsers:
         pass
 
-class AppendStrategy():
-    def getSeparatedUsers(self, usersToAssign: Set[str], usersAlreadyAssigned: Set[str]):
-        pass
+class AppendStrategy(GhiaStrategy):
+    def getGroupedUsers(self, usersAutoMatched: Set[str], usersAlreadyAssigned: Set[str]) -> GroupedUsers:
+        return GroupedUsers(usersAutoMatched=usersAutoMatched.difference(usersAlreadyAssigned), 
+            usersToLeave=usersAlreadyAssigned,
+            usersToRemove=set())
 
-class SetStrategy():
-    def getSeparatedUsers(self, usersToAssign: Set[str], usersAlreadyAssigned: Set[str]):
-        pass
+class SetStrategy(GhiaStrategy):
+    def getGroupedUsers(self, usersAutoMatched: Set[str], usersAlreadyAssigned: Set[str]) -> GroupedUsers:
+        return GroupedUsers(usersAutoMatched=usersAutoMatched, 
+            usersToLeave=usersAlreadyAssigned,
+            usersToRemove=set())
 
-class ChangeStrategy():
-    def getSeparatedUsers(self, usersToAssign: Set[str], usersAlreadyAssigned: Set[str]):
-        pass
-
+class ChangeStrategy(GhiaStrategy):
+    def getGroupedUsers(self, usersAutoMatched: Set[str], usersAlreadyAssigned: Set[str]) -> GroupedUsers:
+        return GroupedUsers(usersAutoMatched=usersAutoMatched.difference(usersAlreadyAssigned), 
+            usersToLeave=usersAlreadyAssigned.intersection(usersAutoMatched),
+            usersToRemove=usersAlreadyAssigned.difference(usersAutoMatched))
 
 class Strategies(Enum):
 	APPEND = AppendStrategy()
