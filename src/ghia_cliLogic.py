8e57e59d52d5bd62c6541232b1ad346278adf754
budikpet <sgt.petrov@gmail.com>
2019-10-08 14:05:33 +0200

    Correct pagination is now used to get data from GitHub API.

--- src/ghia_cliLogic.py
+++ src/ghia_cliLogic.py
@@ -14,7 +14,7 @@ def hasFallbackLabel(issue, context):
 
 def getOutputData(context: GhiaContext, issue, groupedUsers: GroupedUsers):
 	data = None
-	
+
 	if groupedUsers.updateNeeded():
 		data = {
 			"assignees": groupedUsers.getUsersToAssign()
@@ -37,7 +37,7 @@ def updateIssue(context: GhiaContext, issue, groupedUsers: GroupedUsers):
 
 	if not context.dry_run:
 		data = getOutputData(context, issue, groupedUsers)
-		
+
 		if data is not None:
 			r = context.session.patch(f'{context.base}/repos/{context.reposlug}/issues/{issue["number"]}', data=data)
 			statusCode = r.status_code
@@ -68,7 +68,7 @@ def writeLabel(issue, context: GhiaContext):
 def writeOutput(context: GhiaContext, issue, groupedUsers: GroupedUsers, statusCode):
 	info = f'{context.reposlug}#{issue["number"]}'
 	click.echo(f'-> {click.style(info, bold=True)} ({issue["html_url"]})')
-	
+
 	# Create output
 	if statusCode != 200 and not context.dry_run:
 		# Error occured
@@ -79,25 +79,25 @@ def writeOutput(context: GhiaContext, issue, groupedUsers: GroupedUsers, statusC
 				writeUser(userStatus, user)
 		elif context.getFallbackLabel() != "":
 			writeLabel(issue, context)
-	
+
 	print
 
 # Matches patterns to data in appropriate locations
 def patternMatches(issue, location, pattern):
 	result = False
-	
+
 	if location == "title" or location == "any":
 		result = result or bool(re.search(pattern, issue["title"], re.IGNORECASE))
-	
+
 	if location == "text" or location == "any":
 		result = result or bool(re.search(pattern, issue["body"], re.IGNORECASE))
-	
+
 	if location == "label" or location == "any":
 		for label in issue["labels"]:
 			if bool(re.search(pattern, label["name"], re.IGNORECASE)):
 				result = True
 				break
-	
+
 	return result
 
 
@@ -106,7 +106,7 @@ def patternMatches(issue, location, pattern):
 def groupUsers(context: GhiaContext, issue):
 	alreadyAssignedUsers = set(map(lambda assignee: assignee["login"], issue["assignees"]))
 	usersAutoMatched = set()
-	
+
 	# Find users that should be assigned
 	for (location, pairs) in context.getUserPatterns().items():
 		for (pattern, username) in pairs:
@@ -118,43 +118,49 @@ def groupUsers(context: GhiaContext, issue):
 	return context.strategy.getGroupedUsers(usersAutoMatched, alreadyAssignedUsers)
 
 def ghiaRun(strategy, dry_run, config_auth, config_rules, reposlug):
-    """CLI tool for automatic issue assigning of GitHub issues"""
+	"""Run GHIA algorighm to automatically assign GitHub issues"""
 
-    context = GhiaContext("https://api.github.com", strategy, dry_run, config_auth, config_rules, reposlug)
+	context = GhiaContext("https://api.github.com", strategy, dry_run, config_auth, config_rules, reposlug)
 
-    # Load issues
-    context.session = requests.Session()
-    context.session.headers = {
-        'User-Agent': 'Python',
-        'Authorization': f'token {context.getToken()}',
-        # 'Content-Type': 'application/json', 
-        # 'Accept': 'application/json'
-        }
-    context.session.params = {
-        'per_page': 50,
-        'page': 1
-    }
+	# Load issues
+	context.session = requests.Session()
+	context.session.headers = {
+		'User-Agent': 'Python',
+		'Authorization': f'token {context.getToken()}',
+		# 'Content-Type': 'application/json', 
+		# 'Accept': 'application/json'
+		}
+	context.session.params = {
+		'per_page': 50
+	}
 
-    while True:
-        r = context.session.get(f'{context.base}/repos/{reposlug}/issues')
-        issues = r.json()
+	r = context.session.get(f'{context.base}/repos/{reposlug}/issues')
 
-        if r.status_code != 200:
-            click.echo(f'{click.style("ERROR", fg="red")}: Could not list issues for repository {reposlug}', err=True)
-            context.session.close()
-            sys.exit(10)
-        elif len(issues) <= 0:
-            # No more issues exist
-            break
-        
-        # Check all received issues
-        for issue in issues:
-            # Check current issue against all patterns in all locations
-            groupedUsers = groupUsers(context, issue)
-            statusCode = updateIssue(context, issue, groupedUsers)
-            writeOutput(context, issue, groupedUsers, statusCode)
-                    
-        # Next page
-        context.session.params['page'] += 1
+	while True:
+		issues = r.json()
 
-    context.session.close()
+		if r.status_code != 200:
+			click.echo(f'{click.style("ERROR", fg="red")}: Could not list issues for repository {reposlug}', err=True)
+			context.session.close()
+			sys.exit(10)
+		elif len(issues) <= 0:
+			# No more issues exist
+			break
+
+		# Check all received issues
+		for issue in issues:
+			# Check current issue against all patterns in all locations
+			groupedUsers = groupUsers(context, issue)
+			statusCode = updateIssue(context, issue, groupedUsers)
+			writeOutput(context, issue, groupedUsers, statusCode)
+
+		# Next page
+		nextPath = r.links.get("next")
+		if nextPath is not None:
+			r = context.session.get(nextPath["url"])
+			print
+		else:
+			# No more pages exist
+			break
+
+	context.session.close()
