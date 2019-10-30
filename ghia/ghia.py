15916cc8085d13ab28e185e65af2331439acd287
budikpet <sgt.petrov@gmail.com>
2019-10-30 17:42:07 +0100

    GHIA web app now shows properly.

--- ghia/ghia.py
+++ ghia/ghia.py
@@ -1,59 +1,13 @@
 import click
-import requests
-import os
 from flask import Flask
 from typing import Tuple
 from .cli import validator
 from .cli.strategy import Strategies, GhiaContext
 from .cli.ghia_cli_logic import ghia_run
-from .web.root.root_index import bp_root
-
-# Pro testování webhook buď nasadit na pythonanywhere, nebo použít https://requestbin.com/
-
-def get_username(context: GhiaContext):
-	session = requests.Session()
-	session.headers = {
-		'User-Agent': 'Python',
-		'Authorization': f'token {context.get_token()}',
-		}
-
-	r = session.get(f'{context.base}/user')
-
-	return r.json()["login"]
-
-def get_context(config_auth, config_rules) -> GhiaContext:
-	context: GhiaContext = None
-	with open(config_auth) as authFile:
-		with open(config_rules) as rulesFile:
-			dataAuth = validator.validateAuth(None, None, authFile=authFile)
-			dataRules = validator.validateRules(None, None, rulesFile=rulesFile)
-			context = GhiaContext("https://api.github.com", Strategies.APPEND.name, False, dataAuth, dataRules)
-
-	context.username = get_username(context)
-
-	return context
-
-def get_configs() -> Tuple[str, str]:
-	env = os.getenv("GHIA_CONFIG").split(":")
-
-	if "rules" in env[0]:
-		return env[1], env[0]
-	else:
-		return env[0], env[1]
+from .web import ghia_web_logic
 
 def create_app(config=None):
-	app = Flask(__name__)
-
-	config_auth, config_rules = get_configs()
-	context = get_context(config_auth, config_rules)
-
-	app.config["GHIA_CONTEXT"] = context
-	app.secret_key = context.get_secret()
-	app.register_blueprint(bp_root, url_prefix="/")
-
-	# app.config.from_pyfile(config or 'config.py')
-
-	return app
+	return ghia_web_logic.create_app(config=config)
 
 ############################  CLI ###################################
 inputStrategies = [strategy.name.lower() for strategy in Strategies]
