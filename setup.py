2a9aad33e2337dd517e44954a0f5756d697a58c5
budikpet <sgt.petrov@gmail.com>
2019-10-30 12:09:23 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='ghia_budikpet',
+    version='0.3',
+    description='GHIA CLI and web app tutorial.',
+    long_description_content_type="zzz",
+    author='Petr Budík',
+    #author_email='budikpet@...',
+    license='Public Domain',
+    url='https://github.com/budikpet/MI-PYT_GHIA',
+    packages=find_packages(),
+)
