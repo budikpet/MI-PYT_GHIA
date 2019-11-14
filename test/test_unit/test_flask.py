78aa7393a2b83b900ffebb4462ec24a1ec252624
budikpet <sgt.petrov@gmail.com>
2019-11-14 12:42:14 +0100

    Update
    Removed versions.

--- test/test_unit/test_flask.py
+++ test/test_unit/test_flask.py
@@ -1,19 +1,25 @@
 from custom_fixtures import testapp, context
 from ghia.cli.strategy import GhiaContext 
 
-def test_basic(testapp):
-    context: GhiaContext = testapp.application.config["GHIA_CONTEXT"]
-    # assert 'Hello' in testapp.get('/').get_data(as_text=True)
-    assert testapp is not None
+def get_context(app) -> GhiaContext:
+    context: GhiaContext = app.application.config["GHIA_CONTEXT"]
     assert context is not None
 
     assert context.username is not None
     assert context.reposlug is not None
     assert context.base is not None
 
+    return context
+
+def test_basic(testapp):
+    assert testapp is not None
+    context: GhiaContext = get_context(testapp)
+
     data: str = testapp.get('/').get_data(as_text=True)
     
     assert data is not None and data != ""
     assert context.username in data
+    assert f"http://{context.username}.pythonanywhere.com/" in data
+    assert context.get_fallback_label() in data
 
     print
