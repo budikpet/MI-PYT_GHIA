ba3a3deec31db79f68baef7e7cb9eff941871e81
Petr Budik <sgt.petrov@gmail.com>
2019-11-22 15:53:12 +0100

    Added napoleon extension.

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,8 @@ release = '0.4.5.1'
 extensions = [
     "sphinx.ext.doctest",
     "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx"
+    "sphinx.ext.intersphinx",
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
